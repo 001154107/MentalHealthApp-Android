fdfbdabe1cab8b8de1c997504e878d3d7d8dc2c4
nhgiang9992@gmail.com <nhgiang9992@gmail.com>
2022-03-25 13:32:44 +1030

    first commit

--- app/src/main/java/com/a5work/mentalhealthapp/MainActivity.java
+++ app/src/main/java/com/a5work/mentalhealthapp/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //test
+        System.out.println("Giang test");
+
+
     }
 }
