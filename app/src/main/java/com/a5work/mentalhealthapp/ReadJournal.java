b119dc311f564f4e4c65f909565f36519b7c9cbb
001154107 <taron.roberts@student.tafesa.edu.au>
2022-04-26 12:47:43 +0930

    Added Graphing sample
    DOES NOT BUILD YET!
    trying to upgrade the Gradle:
    Upgrade AGP dependency from 7.1.2 to 7.1.3

---
+++ app/src/main/java/com/a5work/mentalhealthapp/ReadJournal.java
@@ -0,0 +1,27 @@
+package com.a5work.mentalhealthapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+public class ReadJournal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GraphView graph = findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+    }
+}
