31e7396dfc4b7f5ce8c2a66f4fe18c4fabecd42c
001154107 <taron.roberts@student.tafesa.edu.au>
2022-06-06 11:49:12 +0930

    Fixed WriteJournal.java, JournalClass.java and ReadJournal.java
    Fixed Graphing and reason display

--- app/src/main/java/com/a5work/mentalhealthapp/ReadJournal.java
+++ app/src/main/java/com/a5work/mentalhealthapp/ReadJournal.java
@@ -2,11 +2,15 @@ package com.a5work.mentalhealthapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,22 +30,23 @@ import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.LinkedList;
 
 public class ReadJournal extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) { // TODO - Make Read from file
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_journal);
 
-        View prevEntry = findViewById(R.id.month_navigation_previous);
-        View nextEntry = findViewById(R.id.month_navigation_next);
-        View todaysDate = findViewById(R.id.todaysDateTV);
-        View entryDate = findViewById(R.id.EntryDate);
-        View reason = findViewById(R.id.JournalText);
+        ImageButton prevEntry = findViewById(R.id.month_navigation_previous);
+        ImageButton nextEntry = findViewById(R.id.month_navigation_next);
+        TextView entryDate = findViewById(R.id.EntryDate);
+        TextView reason = findViewById(R.id.JournalText);
         GraphView graph = findViewById(R.id.graph);
 
 
@@ -73,15 +78,30 @@ public class ReadJournal extends AppCompatActivity {
         }
 
         for(JournalClass i : entrieslinkedList){
-            physicalDataPoints[entrieslinkedList.indexOf(i)] = new DataPoint(entrieslinkedList.indexOf(i),i.getMentalRating());
+            physicalDataPoints[entrieslinkedList.indexOf(i)] = new DataPoint(entrieslinkedList.indexOf(i),i.getPhysicalRating());
         }
 
         LineGraphSeries<DataPoint> emotionalRating = new LineGraphSeries<>(emotionDataPoints);
-        LineGraphSeries<DataPoint> physicalRating= new LineGraphSeries<>(physicalDataPoints);
+        LineGraphSeries<DataPoint> physicalRating = new LineGraphSeries<>(physicalDataPoints);
         graph.addSeries(physicalRating);
         graph.addSeries(emotionalRating);
+        //emotionalRating.setTitle("Emotional Ratings");
+        emotionalRating.setColor(0xffce93d8);
+        //physicalRating.setTitle("Physical Ratings");
+        physicalRating.setColor(0xff81d4fa);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            readDateAndReason(0,entryDate, reason, entrieslinkedList);
+        }
+    }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void readDateAndReason(int i, TextView entryDate, TextView reason, LinkedList<JournalClass> entrieslinkedList) {
+        // REASON and DATE
+        String formattedDate = entrieslinkedList.get(i).getDate().format(DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm a"));
+
+        reason.setText(entrieslinkedList.get(i).getReason());
+        entryDate.setText(formattedDate);
     }
 
 
